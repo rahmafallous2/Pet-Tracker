d5147ddf00d5893b4f03901d92f0fdd9529cb394
rahmafallous2 <rahmifallous@gmail.com>
2025-07-01 01:36:35 +0300

    Creating a list of pets and defining a function to get the non-adopted pets

--- pts.py
+++ pts.py
@@ -1,3 +1,4 @@
+#Part1
 class Pet:
     def __init__(self, name, species,age,adopted=False):
         self.name = name
@@ -26,3 +27,12 @@ pet3.birthday()
 pet1.display_info()
 pet2.display_info()
 pet3.display_info()
+
+#Part2
+pets = [pet1, pet2, pet3]
+def get_non_Adopted():
+    non_adopted = []
+    for pet in pets:
+        if pet.adopted == False:
+            non_adopted.append(pet)
+    return non_adopted
